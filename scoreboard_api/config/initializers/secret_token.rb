88e0c14d17287f756b1c779d1e69f6b5ee9c542d
Enrique Urzua <eurzua@digitalartsnetwork.com.mx>
2013-06-24 16:25:32 -0500

    Add rails 3.2.13 framework

---
+++ scoreboard_api/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreboardApi::Application.config.secret_token = '26a523e7f5ddd44f3b90c509a18d582d86a8685d2ff2e50d543ded4dd9439eff6c6bc2ff2890249a84dec49f411138f4e15d1600d69fd9841c7f08a8173e4f9a'

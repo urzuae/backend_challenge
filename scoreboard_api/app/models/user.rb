4fcd1ef3a4b5cac747d4c99b0ca8d1dd83fa5a95
urzuae <urzuae@gmail.com>
2013-07-02 01:07:51 -0500

    Add first test samples

--- scoreboard_api/app/models/user.rb
+++ scoreboard_api/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   
   attr_accessible :name
   
-  has_many :game_players, foreign_key: :player, dependent: :destroy
+  has_many :game_players, foreign_key: :player_id, dependent: :destroy
   has_many :games_won, foreign_key: "winner", dependent: :destroy, class_name: "Game"
   
   validates :name, presence: true
---
+++ scoreboard_api/spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe User do
+  
+  describe "relationships" do
+    it { should have_many(:game_players) }
+  end
+  
+  describe "attr_accesible attributes" do
+    it { should allow_mass_assignment_of(:name) }
+  end
+
+  describe "protected attributes from mass assignment" do
+    it { should_not allow_mass_assignment_of(:wins_count) }
+    it { should_not allow_mass_assignment_of(:loses_count) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
+
+end
